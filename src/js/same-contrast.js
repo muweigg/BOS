c77cc353efdc4e03ceb03cf4ccd171f025d02401
[muweigg] <muweigg@gmail.com>
2018-10-27 20:59:09 +0800

    update

---
+++ src/js/same-contrast.js
@@ -0,0 +1,19 @@
+import '../css/common/common.scss';
+
+import Vue from 'vue';
+
+new Vue({
+  el: '.container-fluid',
+  mounted () {
+    const datatable = $('#responsive-datatable');
+    datatable.DataTable({
+      lengthChange: false,
+      searching: false,
+      language: {
+        info: "显示第 _START_ 到 _END_ 条，共 _TOTAL_ 条",
+        infoEmpty: "当前没有任何数据",
+      }
+    });
+  }
+});
+
