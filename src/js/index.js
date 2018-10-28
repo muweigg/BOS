25aedad034d5ff716445a6420d225b2f7e33e8d8
[muweigg] <muweigg@gmail.com>
2018-10-28 15:16:31 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ import * as Echarts from 'echarts';
 $(() => {
   initBMap();
   initLineChart();
+  initPieChart();
+  initRadarChart();
 });
 
 const initBMap = () => {
@@ -36,7 +38,8 @@ const initLineChart = () => {
       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     },
     yAxis: {
-      type: 'value'
+      type: 'value',
+      splitLine: false,
     },
     series: [{
         name: '工业总产值',
@@ -59,6 +62,178 @@ const initLineChart = () => {
     ]
   };
   const dom = document.querySelector(".line-chart");
+  const myChart = Echarts.init(dom);
+  myChart.setOption(option, true);
+  return myChart;
+}
+
+const initPieChart = () => {
+  const data = {
+    legendData: [
+      '农、牧、林、渔',
+      '采矿和采石',
+      '制造',
+      '点、煤气、蒸汽和空调供应',
+      '建筑',
+      '其他',
+    ],
+    selected: {
+      ['农、牧、林、渔']: true,
+      ['采矿和采石']: true,
+      ['制造']: true,
+      ['点、煤气、蒸汽和空调供应']: true,
+      ['建筑']: true,
+      ['其他']: true,
+    },
+    seriesData: [{
+        name: "农、牧、林、渔",
+        value: 37757
+      },
+      {
+        name: "采矿和采石",
+        value: 6358
+      },
+      {
+        name: "制造",
+        value: 87502
+      },
+      {
+        name: "点、煤气、蒸汽和空调供应",
+        value: 5702
+      },
+      {
+        name: "建筑",
+        value: 30622
+      },
+      {
+        name: "其他",
+        value: 92045
+      },
+    ]
+  };
+  const option = {
+    textStyle: {
+      color: '#fff',
+    },
+    title: {
+      text: '工业总产值（亿元）',
+      x: 'center',
+      textStyle: {
+        color: '#fff',
+      },
+    },
+    legend: {
+      type: 'scroll',
+      orient: 'vertical',
+      right: 10,
+      top: 20,
+      bottom: 20,
+      data: data.legendData,
+      selected: data.selected,
+      textStyle: {
+        color: '#fff',
+      },
+    },
+    series: [{
+      // name: '姓名',
+      type: 'pie',
+      radius: '55%',
+      center: ['40%', '50%'],
+      data: data.seriesData,
+      itemStyle: {
+        emphasis: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      },
+      label: {
+        normal: {
+          show: false
+        },
+        emphasis: {
+          show: true
+        }
+      },
+      lableLine: {
+        normal: {
+          show: false
+        },
+        emphasis: {
+          show: true
+        }
+      },
+    }]
+  };
+  const dom = document.querySelector(".pie-chart");
+  const myChart = Echarts.init(dom);
+  myChart.setOption(option, true);
+  return myChart;
+}
+
+const initRadarChart = () => {
+  const option = option = {
+    legend: {
+      type: 'scroll',
+      orient: 'vertical',
+      right: 10,
+      top: 20,
+      bottom: 20,
+      textStyle: {
+        color: '#fff',
+      },
+      data: ['木耳物流园', '两江新区']
+    },
+    radar: {
+      // shape: 'circle',
+      name: {
+        textStyle: {
+          color: '#fff',
+          // backgroundColor: '#999',
+          // borderRadius: 3,
+          // padding: [3, 5]
+        }
+      },
+      indicator: [{
+          name: '工业生产总值',
+          max: 6500
+        },
+        {
+          name: '从业人员数',
+          max: 16000
+        },
+        {
+          name: '居民消费支出',
+          max: 30000
+        },
+        {
+          name: '财政收入',
+          max: 38000
+        },
+        {
+          name: '固定投资总额',
+          max: 52000
+        },
+      ],
+      radius: '70%',
+      center: ['50%', '50%'],
+    },
+    series: [{
+      name: '木耳物流园 vs 两江新区',
+      type: 'radar',
+      // areaStyle: {normal: {}},
+      data: [{
+          value: [4300, 10000, 28000, 35000, 50000],
+          name: '木耳物流园'
+        },
+        {
+          value: [5000, 14000, 28000, 31000, 42000],
+          name: '两江新区'
+        }
+      ]
+    }]
+  };
+  const dom = document.querySelector(".radar-chart");
   const myChart = Echarts.init(dom);
   myChart.setOption(option, true);
   return myChart;
