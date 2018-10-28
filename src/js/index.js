73f1a795bd86896632d66737409a2a06dbedf574
[muweigg] <muweigg@gmail.com>
2018-10-28 12:19:57 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,13 @@
 import '../css/common/common.scss';
+import * as BMapConfig from './custom_map_config.json';
 
-// import 'script-loader!./common/third-party/ubold/jquery.min';
-// import 'script-loader!./common/third-party/ubold/popper.min';
-// import 'script-loader!./common/third-party/ubold/bootstrap.min';
-// import 'script-loader!./common/third-party/ubold/wow.min';
-// import 'script-loader!./common/third-party/ubold/jquery.scrollTo.min';
-// import 'script-loader!./common/third-party/ubold/waypoints/lib/jquery.waypoints.min';
-// import 'script-loader!./common/third-party/ubold/counterup/jquery.counterup.min';
-// import 'script-loader!./common/third-party/ubold/morris/morris.min';
-// import 'script-loader!./common/third-party/ubold/raphael/raphael-min';
-// import 'script-loader!babel-loader!./common/third-party/ubold/detect';
-// import 'script-loader!babel-loader!./common/third-party/ubold/fastclick';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery.slimscroll';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery.blockUI';
-// import 'script-loader!babel-loader!./common/third-party/ubold/waves';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery.nicescroll';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery-knob/jquery.knob';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery.core';
-// import 'script-loader!babel-loader!./common/third-party/ubold/jquery.app';
+$(() => {
+  initBMap();
+});
 
+const initBMap = () => {
+  const map = new BMap.Map("bmap");
+  const point = new BMap.Point(116.404, 39.915);
+  map.centerAndZoom(point, 15);
+  map.setMapStyleV2({styleJson: BMapConfig});
+}
