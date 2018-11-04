32d8867ae2aa46e82e7f7665abe8771c5484ba30
muweigg <muweigg@gmail.com>
2018-11-04 18:19:24 +0800

    update

--- src/js/integrated-data-query.js
+++ src/js/integrated-data-query.js
@@ -1,3 +1,14 @@
 import '../css/common/common.scss';
 
 import Vue from 'vue';
+
+new Vue({
+  el: '.integrated-data-query',
+  data: {
+    queryType: 0,
+    areaType: 0,
+  },
+  mounted () {
+
+  },
+});
