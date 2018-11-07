d9c16e9a60341a584641bbd55df4bcd5646026c8
[muweigg] <muweigg@gmail.com>
2018-11-07 18:03:46 +0800

    update

--- src/js/integrated-data-query.js
+++ src/js/integrated-data-query.js
@@ -9,6 +9,48 @@ new Vue({
     areaType: 0,
   },
   mounted () {
+    this.initTree();
+  },
+  methods: {
+    initTree () {
 
+      const queryData = [
+        {
+          text: '国民经济核算',
+          state: {
+            opened: true,
+            // selected: true
+          },
+          children: [
+            {
+              text: '地区生成总值',
+              state: {
+                selected: true
+              },
+            }
+          ]
+        },
+        '能源',
+        '国内贸易',
+        '利用内资',
+        {
+          text: '财政',
+          children: [
+            '财政收入',
+            '财政支出',
+          ]
+        },
+        {
+          text: '人民生活',
+          children: [
+            '居民人均可支配收入',
+            '常住居民人均可支配收入',
+          ]
+        },
+      ];
+
+      $('.query-tree').jstree({core: { data: queryData }});
+
+    }
   },
 });
